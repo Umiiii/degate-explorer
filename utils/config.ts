9cfe969b4516f738414559f30f2c41f7f5dded48
john.li <john.li@loopring.io>
2023-02-27 16:45:28 +0800

    reverse config

--- utils/config.ts
+++ utils/config.ts
@@ -1,7 +1,7 @@
 // TODO: remove once env variables are added to vercel.
-export const LOOPRING_SUBGRAPH = 'https://api.thegraph.com/subgraphs/name/loopring/loopring'
-  // process.env.NEXT_PUBLIC_SUBGRAPH_ENDPOINT ??
-  // 'https://gateway.thegraph.com/api/294a874dfcbae25bcca653a7f56cfb63/subgraphs/id/7QP7oCLbEAjejkp7wSLTD1zbRMSiDydAmALksBB5E6i1';
+export const LOOPRING_SUBGRAPH =
+  process.env.NEXT_PUBLIC_SUBGRAPH_ENDPOINT ??
+  'https://gateway.thegraph.com/api/294a874dfcbae25bcca653a7f56cfb63/subgraphs/id/7QP7oCLbEAjejkp7wSLTD1zbRMSiDydAmALksBB5E6i1';
 export const EXPLORER_URL = 'https://etherscan.io/';
 export const INFURA_ENDPOINT =
   process.env.NEXT_PUBLIC_INFURA_ENDPOINT ?? 'https://mainnet.infura.io/v3/873030883ea1405898041be94ab2bfd5';
