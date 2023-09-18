ecd8d2275b11558a493485802ef2091d0ea48a32
umi <9@umi.cat>
2023-09-18 15:23:00 +0800

    add tmp holder

--- components/NetworkStats.tsx
+++ components/NetworkStats.tsx
@@ -33,29 +33,28 @@ const NetworkStats: React.FC<{ networkStats: any }> = ({ networkStats }) => {
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-16">
       <div className="flex flex-col px-8 py-4 rounded-xl pb-10 border-2 border-loopring-blue dark:border-loopring-dark-gray text-loopring-lightBlue dark:text-white items-center justify-center h-32">
         <span className=" mb-4">Total Transactions</span>
-        <span className="text-3xl flex-1">{numeral(networkStats.proxy.transactionCount).format('0,0')}</span>
+        {/*<span className="text-3xl flex-1">{numeral(networkStats.proxy.transactionCount).format('0,0')}</span>*/}
+          <span className="text-3xl flex-1">WIP</span>
       </div>
       <div className="flex flex-col px-8 py-2 rounded-xl pb-10 border-2 border-loopring-blue dark:border-loopring-dark-gray text-loopring-lightBlue dark:text-white items-center justify-center h-32">
         <span className=" mb-4">Total Blocks</span>
-        <span className="text-3xl flex-1">{numeral(networkStats.proxy.blockCount).format('0,0')}</span>
+          <span className="text-3xl flex-1">WIP</span>
       </div>
       <div className="flex flex-col px-8 py-2 rounded-xl pb-10 border-2 border-loopring-blue dark:border-loopring-dark-gray text-loopring-lightBlue dark:text-white items-center justify-center h-32">
         <span className=" mb-4">Total L2 Accounts</span>
-        <span className="text-3xl flex-1">{networkStats && numeral(networkStats.proxy.userCount).format('0,0')}</span>
+          <span className="text-3xl flex-1">WIP</span>
       </div>
       <div className="flex flex-col px-8 py-2 rounded-xl pb-10 border-2 border-loopring-blue dark:border-loopring-dark-gray text-loopring-lightBlue dark:text-white items-center justify-center h-32">
         <span className=" mb-4">Avg. Block Time</span>
-        <span className="text-3xl flex-1">{avgBlockDetails.timeBetweenBlocks}</span>
+          <span className="text-3xl flex-1">WIP</span>
       </div>
       <div className="flex flex-col px-8 py-2 rounded-xl pb-10 border-2 border-loopring-blue dark:border-loopring-dark-gray text-loopring-lightBlue dark:text-white items-center justify-center h-32">
         <span className=" mb-4">Avg. Txs per Block</span>
-        <span className="text-3xl flex-1">
-          {avgBlockDetails.transactionCount && numeral(avgBlockDetails.transactionCount).format('0,0')}
-        </span>
+          <span className="text-3xl flex-1">WIP</span>
       </div>
       <div className="flex flex-col px-8 py-2 rounded-xl pb-10 border-2 border-loopring-blue dark:border-loopring-dark-gray text-loopring-lightBlue dark:text-white items-center justify-center h-32">
         <span className=" mb-4">Last Block Submitted</span>
-        <span className="text-3xl flex-1">{networkStats && getTimeFromNow(networkStats.blocks[0].timestamp)}</span>
+          <span className="text-3xl flex-1">WIP</span>
       </div>
     </div>
   );
